bfc08a5e104a7e5711e8d17802b8528476239565
Archit Pattanaik <98776672+archit-2003@users.noreply.github.com>
2023-04-02 22:02:57 +0530

    Delete customauth directory

--- customauth/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from customauth.models import UserAccount,Stock,WatchList
-admin.site.register(UserAccount)
-admin.site.register(Stock)
-admin.site.register(WatchList)
-
